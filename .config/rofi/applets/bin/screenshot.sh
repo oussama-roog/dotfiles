7ae59a9613ef23a31333e8289146c05bcb49bb89
oussama-roog <oussamaroog@gmail.com>
2025-02-26 10:35:59 +0100

    Add screenshot to rofi and hyprland

--- .config/rofi/applets/bin/screenshot.sh
+++ .config/rofi/applets/bin/screenshot.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/env bash
 
 ## Author  : Aditya Shakya (adi1090x)
 ## Github  : @adi1090x
@@ -14,152 +14,151 @@ prompt='Screenshot'
 mesg="DIR: `xdg-user-dir PICTURES`/Screenshots"
 
 if [[ "$theme" == *'type-1'* ]]; then
-	list_col='1'
-	list_row='5'
-	win_width='400px'
+    list_col='1'
+    list_row='5'
+    win_width='400px'
 elif [[ "$theme" == *'type-3'* ]]; then
-	list_col='1'
-	list_row='5'
-	win_width='120px'
+    list_col='1'
+    list_row='5'
+    win_width='120px'
 elif [[ "$theme" == *'type-5'* ]]; then
-	list_col='1'
-	list_row='5'
-	win_width='520px'
+    list_col='1'
+    list_row='5'
+    win_width='520px'
 elif [[ ( "$theme" == *'type-2'* ) || ( "$theme" == *'type-4'* ) ]]; then
-	list_col='5'
-	list_row='1'
-	win_width='670px'
+    list_col='5'
+    list_row='1'
+    win_width='670px'
 fi
 
 # Options
 layout=`cat ${theme} | grep 'USE_ICON' | cut -d'=' -f2`
 if [[ "$layout" == 'NO' ]]; then
-	option_1=" Capture Desktop"
-	option_2=" Capture Area"
-	option_3=" Capture Window"
-	option_4=" Capture in 5s"
-	option_5=" Capture in 10s"
+    option_1=" Capture Desktop"
+    option_2=" Capture Area"
+    option_3=" Capture Window"
+    option_4=" Capture in 5s"
+    option_5=" Capture in 10s"
 else
-	option_1=""
-	option_2=""
-	option_3=""
-	option_4=""
-	option_5=""
+    option_1=""
+    option_2=""
+    option_3=""
+    option_4=""
+    option_5=""
 fi
 
 # Rofi CMD
 rofi_cmd() {
-	rofi -theme-str "window {width: $win_width;}" \
-		-theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "";}' \
-		-dmenu \
-		-p "$prompt" \
-		-mesg "$mesg" \
-		-markup-rows \
-		-theme ${theme}
+    rofi -theme-str "window {width: $win_width;}" \
+        -theme-str "listview {columns: $list_col; lines: $list_row;}" \
+        -theme-str 'textbox-prompt-colon {str: "";}' \
+        -dmenu \
+        -p "$prompt" \
+        -mesg "$mesg" \
+        -markup-rows \
+        -theme ${theme}
 }
 
 # Pass variables to rofi dmenu
 run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
+    echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
 }
 
 # Screenshot
-time=`date +%Y-%m-%d-%H-%M-%S`
-geometry=`xrandr | grep 'current' | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current'`
+time=$(date +%Y-%m-%d-%H-%M-%S)
+geometry=$(hyprctl monitors | grep "active" | cut -d ',' -f 2 | tr -d '[:space:]')
 dir="`xdg-user-dir PICTURES`/Screenshots"
 file="Screenshot_${time}_${geometry}.png"
 
 if [[ ! -d "$dir" ]]; then
-	mkdir -p "$dir"
+    mkdir -p "$dir"
 fi
 
 # notify and view screenshot
 notify_view() {
-	notify_cmd_shot='dunstify -u low --replace=699'
-	${notify_cmd_shot} "Copied to clipboard."
-	viewnior ${dir}/"$file"
-	if [[ -e "$dir/$file" ]]; then
-		${notify_cmd_shot} "Screenshot Saved."
-	else
-		${notify_cmd_shot} "Screenshot Deleted."
-	fi
+    notify_cmd_shot='dunstify -u low --replace=699'
+    ${notify_cmd_shot} "Copied to clipboard."
+    # imv "${dir}/${file}" & # Run in background.  Change imv if needed.
+    if [[ -e "$dir/$file" ]]; then
+        ${notify_cmd_shot} "Screenshot Saved."
+    else
+        ${notify_cmd_shot} "Screenshot Deleted."
+    fi
 }
 
 # Copy screenshot to clipboard
-copy_shot () {
-	tee "$file" | xclip -selection clipboard -t image/png
+copy_shot() {
+  wl-clipboard -t image/png < "$file"
 }
 
 # countdown
-countdown () {
-	for sec in `seq $1 -1 1`; do
-		dunstify -t 1000 --replace=699 "Taking shot in : $sec"
-		sleep 1
-	done
+countdown() {
+    for sec in `seq $1 -1 1`; do
+        dunstify -t 1000 --replace=699 "Taking shot in : $sec"
+        sleep 1
+    done
 }
 
 # take shots
-shotnow () {
-	cd ${dir} && sleep 0.5 && maim -u -f png | copy_shot
-	notify_view
+shotnow() {
+    cd "${dir}" && sleep 0.5 && grim "${dir}/${file}" && copy_shot
+    notify_view
 }
 
-shot5 () {
-	countdown '5'
-	sleep 1 && cd ${dir} && maim -u -f png | copy_shot
-	notify_view
+shot5() {
+    countdown '5'
+    sleep 1 && cd "${dir}" && grim "${dir}/${file}" && copy_shot
+    notify_view
 }
 
-shot10 () {
-	countdown '10'
-	sleep 1 && cd ${dir} && maim -u -f png | copy_shot
-	notify_view
+shot10() {
+    countdown '10'
+    sleep 1 && cd "${dir}" && grim "${dir}/${file}" && copy_shot
+    notify_view
 }
 
-shotwin () {
-	cd ${dir} && maim -u -f png -i `xdotool getactivewindow` | copy_shot
-	notify_view
+shotwin() {
+    window_id=$(hyprctl activewindow | grep "window:" | cut -d ':' -f 2 | tr -d ' ')
+    cd "${dir}" && grim -w "$window_id" "${dir}/${file}" && copy_shot
+    notify_view
 }
 
-shotarea () {
-	cd ${dir} && maim -u -f png -s -b 2 -c 0.35,0.55,0.85,0.25 -l | copy_shot
-	notify_view
+shotarea() {
+    cd "${dir}" && grim -g "$(slurp)" "${dir}/${file}" && copy_shot
+    notify_view
 }
 
 # Execute Command
 run_cmd() {
-	if [[ "$1" == '--opt1' ]]; then
-		shotnow
-	elif [[ "$1" == '--opt2' ]]; then
-		shotarea
-	elif [[ "$1" == '--opt3' ]]; then
-		shotwin
-	elif [[ "$1" == '--opt4' ]]; then
-		shot5
-	elif [[ "$1" == '--opt5' ]]; then
-		shot10
-	fi
+    if [[ "$1" == '--opt1' ]]; then
+        shotnow
+    elif [[ "$1" == '--opt2' ]]; then
+        shotarea
+    elif [[ "$1" == '--opt3' ]]; then
+        shotwin
+    elif [[ "$1" == '--opt4' ]]; then
+        shot5
+    elif [[ "$1" == '--opt5' ]]; then
+        shot10
+    fi
 }
 
 # Actions
 chosen="$(run_rofi)"
 case ${chosen} in
     $option_1)
-		run_cmd --opt1
+        run_cmd --opt1
         ;;
     $option_2)
-		run_cmd --opt2
+        run_cmd --opt2
         ;;
     $option_3)
-		run_cmd --opt3
+        run_cmd --opt3
         ;;
     $option_4)
-		run_cmd --opt4
+        run_cmd --opt4
         ;;
     $option_5)
-		run_cmd --opt5
+        run_cmd --opt5
         ;;
 esac
-
-
