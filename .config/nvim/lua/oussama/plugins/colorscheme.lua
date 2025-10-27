efe6db7ccd269ccf210285b6fa879098a4725a08
oussama-roog <oussamaroog@gmail.com>
2025-10-27 10:47:22 +0100

    Nix: before adding theme switcher

--- .config/nvim/lua/oussama/plugins/colorscheme.lua
+++ .config/nvim/lua/oussama/plugins/colorscheme.lua
@@ -1,93 +1,14 @@
 return {
 	{
-		"projekt0n/github-nvim-theme",
-		lazy = false, -- make sure we load this during startup if it is your main colorscheme
-		priority = 1000, -- make sure to load this before all the other start plugins
-
-		config = function()
-			require("github-theme").setup({
-				options = {
-					transparent = true,
-				},
-			})
-
-			-- vim.cmd("colorscheme github_dark_dimmed")
-		end,
-	},
-
-	{
-		"catppuccin/nvim",
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			require("catppuccin").setup({
-				flavour = "mocha", -- latte, frappe, macchiato, mocha
-				transparent_background = true,
-			})
-		end,
-	},
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function()
-			require("tokyonight").setup({
-				style = "night", -- The theme comes in three styles, `storm`, `moon`, and `night`.
-				transparent = true, -- Enable this to disable setting the background color
-			})
-		end,
-	},
-	{
-		"navarasu/onedark.nvim",
-		priority = 1000,
-		opts = {},
-		config = function()
-			require("onedark").setup({
-				style = "dark",
-				transparent = true, -- Enable this to disable setting the background color
-			})
-		end,
-	},
-
-	{
-		"bluz71/vim-moonfly-colors",
-		name = "moonfly",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			-- vim.cmd("colorscheme moonfly")
-		end,
-	},
-
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-		config = function()
-			-- vim.cmd("colorscheme rose-pine-main")
-		end,
-	},
-
-	{
 		"vague2k/vague.nvim",
 		config = function()
-			-- NOTE: you do not need to call setup if you don't want to.
-			require("vague").setup({
-				-- optional configuration here
-			})
-
-			vim.cmd("colorscheme vague")
+			-- vim.cmd("colorscheme vague")
 		end,
 	},
-
 	{
-		"tahayvr/matteblack.nvim",
-		lazy = false,
-		priority = 1000,
+		"tinted-theming/tinted-vim",
 		config = function()
-			-- vim.cmd.colorscheme("matteblack")
+			vim.cmd.colorscheme("base16-black-metal-bathory")
 		end,
 	},
-
-	{ "EdenEast/nightfox.nvim" }, -- lazy
 }
