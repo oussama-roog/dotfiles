dd62fd41de9e3d7dd741e7277ec67fa0cd425ca7
oussama-roog <oussamaroog@gmail.com>
2025-10-28 16:08:00 +0100

    Nvim: add kulala plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,4 +2,6 @@ require("oussama.core")
 require("oussama.lazy")
 require("oussama.core.theme")
 
+vim.api.nvim_set_hl(0, "LspReferenceText", { bg = "NONE" })
+vim.api.nvim_set_hl(0, "LspReferenceRead", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "LspReferenceWrite", { bg = "NONE" })
